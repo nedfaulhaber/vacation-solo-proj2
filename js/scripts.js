204f0708f8e3bcefe1569c35de8727fd73171e07
Ned <nedfaulhaber@gmail.com>
2016-03-25 12:06:05 -0700

    begin to add js for color question

---
+++ js/scripts.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+  var colorPoints = 0;
+  var chocolatePoints = 0;
+  var bathPoints = 0;
+  var hogwartsPoints = 0;
+  var ghostPoints = 0;
+
+  $("#inputColor").change(function() {
+    var color = ($("#inputColor").val()).toLowerCase();
+
+    if (color === "red") {
+      colorPoints = 5;
+    }
+
+    else if (color === "orange") {
+      colorPoints = 8;
+    }
+
+    else if (color === "yellow") {
+      colorPoints = 8;
+    }
+
+    else if (color === "green") {
+      colorPoints = 8;
+    }
+
+    else if (color === "blue") {
+      colorPoints = 8;
+    }
+
+    else if (color === "indigo") {
+      colorPoints = 8;
+    }
+
+    else if (color === "violet") {
+      colorPoints = 8;
+    }
+
+    else {
+      alert("Not a valid response! Please choose from the possible answers provided");
+    }
+
+  });
+
+    $("#inputColor").change(function() {
+      var color = ($("#inputColor").val()).toLowerCase();
+
+      if (color === "red") {
+        colorPoints = 5;
+      }
+
+      else if (color === "orange") {
+        colorPoints = 8;
+      }
+
+      else if (color === "yellow") {
+        colorPoints = 8;
+      }
+
+      else if (color === "green") {
+        colorPoints = 8;
+      }
+
+      else if (color === "blue") {
+        colorPoints = 8;
+      }
+
+      else if (color === "indigo") {
+        colorPoints = 8;
+      }
+
+      else if (color === "violet") {
+        colorPoints = 8;
+      }
+
+      else {
+        alert("Not a valid response! Please choose from the possible answers provided");
+      }
+  });
+
+});  
