7b820a8ca67d94c6dc335bb55c00c6a9f98905e0
Ned <nedfaulhaber@gmail.com>
2016-03-25 12:14:12 -0700

    finish js for color, add js for chocolate

--- js/scripts.js
+++ js/scripts.js
@@ -9,73 +9,56 @@ $(document).ready(function() {
     var color = ($("#inputColor").val()).toLowerCase();
 
     if (color === "red") {
-      colorPoints = 5;
+      colorPoints = 1;
     }
 
     else if (color === "orange") {
-      colorPoints = 8;
+      colorPoints = 2;
     }
 
     else if (color === "yellow") {
-      colorPoints = 8;
+      colorPoints = 3;
     }
 
     else if (color === "green") {
-      colorPoints = 8;
+      colorPoints = 4;
     }
 
     else if (color === "blue") {
-      colorPoints = 8;
+      colorPoints = 5;
     }
 
     else if (color === "indigo") {
-      colorPoints = 8;
+      colorPoints = 6;
     }
 
     else if (color === "violet") {
-      colorPoints = 8;
+      colorPoints = 7;
     }
 
     else {
       alert("Not a valid response! Please choose from the possible answers provided");
     }
-
   });
 
-    $("#inputColor").change(function() {
-      var color = ($("#inputColor").val()).toLowerCase();
+    $("#inputChocolate").change(function() {
+      var chocolate = ($("#inputChocolate").val()).toLowerCase();
 
-      if (color === "red") {
-        colorPoints = 5;
+      if (chocolate === "dark chocolate") {
+        chocolatePoints = 1;
       }
 
-      else if (color === "orange") {
-        colorPoints = 8;
+      else if (chocolate === "milk chocolate") {
+        chocolatePoints = 2;
       }
 
-      else if (color === "yellow") {
-        colorPoints = 8;
-      }
-
-      else if (color === "green") {
-        colorPoints = 8;
-      }
-
-      else if (color === "blue") {
-        colorPoints = 8;
-      }
-
-      else if (color === "indigo") {
-        colorPoints = 8;
-      }
-
-      else if (color === "violet") {
-        colorPoints = 8;
+      else if (chocolate === "white chocolate") {
+        chocolatePoints = 3;
       }
 
       else {
         alert("Not a valid response! Please choose from the possible answers provided");
       }
-  });
+    });
 
-});  
+});
